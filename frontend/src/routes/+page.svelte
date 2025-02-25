a7e9a7ac62f99ef44dcc1d09cf8000894e2cc9c0
kalcow <81459450+kalcow@users.noreply.github.com>
2025-02-25 00:15:19 -0800

    adding core structure of web ui

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,49 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Calculator from 'lucide-svelte/icons/calculator';
+	import Calendar from 'lucide-svelte/icons/calendar';
+	import CreditCard from 'lucide-svelte/icons/credit-card';
+	import Settings from 'lucide-svelte/icons/settings';
+	import Smile from 'lucide-svelte/icons/smile';
+	import User from 'lucide-svelte/icons/user';
+
+	import * as Command from '$lib/components/ui/command/index.js';
+</script>
+
+<Command.Root class="max-w-[450px] rounded-lg border shadow-md">
+	<Command.List>
+		<Command.Empty>No results found.</Command.Empty>
+		<Command.Group heading="Suggestions">
+			<Command.Item>
+				<Calendar />
+				<span>Calendar</span>
+			</Command.Item>
+			<Command.Item>
+				<Smile />
+				<span>Search Emoji</span>
+			</Command.Item>
+			<Command.Item disabled>
+				<Calculator />
+				<span>Calculator</span>
+			</Command.Item>
+		</Command.Group>
+		<Command.Separator />
+		<Command.Group heading="Settings">
+			<Command.Item>
+				<User />
+				<span>Profile</span>
+				<Command.Shortcut>⌘P</Command.Shortcut>
+			</Command.Item>
+			<Command.Item>
+				<CreditCard />
+				<span>Billing</span>
+				<Command.Shortcut>⌘B</Command.Shortcut>
+			</Command.Item>
+			<Command.Item>
+				<Settings />
+				<span>Settings</span>
+				<Command.Shortcut>⌘S</Command.Shortcut>
+			</Command.Item>
+		</Command.Group>
+	</Command.List>
+	<Command.Input placeholder="Type a command or search..." />
+</Command.Root>
---
+++ frontend/src/routes/session/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import * as Resizable from '$lib/components/ui/resizable';
+	import { Textarea } from '$lib/components/ui/textarea';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+</script>
+
+<div class="flex h-screen w-screen items-center justify-center">
+	<Resizable.PaneGroup direction="horizontal" class="max-w-full border">
+		<Resizable.Pane defaultSize={25} minSize={25} class="relative">
+			<div
+				class="bg-background absolute bottom-0 z-10 flex h-32 w-full items-center justify-center border-t p-4"
+			>
+				<DropdownMenu.Root>
+					<DropdownMenu.Trigger>Open</DropdownMenu.Trigger>
+					<DropdownMenu.Content>
+						<DropdownMenu.Group>
+							<DropdownMenu.DropdownMenuGroupHeading
+								>My Account</DropdownMenu.DropdownMenuGroupHeading
+							>
+							<DropdownMenu.Separator />
+							<DropdownMenu.Item>Profile</DropdownMenu.Item>
+							<DropdownMenu.Item>Billing</DropdownMenu.Item>
+							<DropdownMenu.Item>Team</DropdownMenu.Item>
+							<DropdownMenu.Item>Subscription</DropdownMenu.Item>
+						</DropdownMenu.Group>
+					</DropdownMenu.Content>
+				</DropdownMenu.Root>
+				<Textarea class="h-full resize-none" placeholder="Enter prompt here" />
+			</div>
+		</Resizable.Pane>
+		<Resizable.Handle />
+		<Resizable.Pane defaultSize={75} minSize={50}>
+			<div class="flex h-full items-center justify-center p-6">
+				<span class="font-semibold">Two</span>
+			</div>
+		</Resizable.Pane>
+	</Resizable.PaneGroup>
+</div>
